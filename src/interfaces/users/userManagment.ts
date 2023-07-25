be403bfe2d5aacbe8ffd4e8927011c461ca61e8a
Afraz Malik <afrazmalik321@gmail.com>
2023-07-26 01:23:53 +0500

    "User Management Crud in process"

--- src/interfaces/users/userManagment.ts
+++ src/interfaces/users/userManagment.ts
@@ -1,13 +1,16 @@
+import { assignShop, customerGroup, role } from '../general';
+
 export interface IuserManagementTableRow {
+  fullName: string;
   userName: string;
   phone: string;
-  role: string; // roleId, enum of role
-  assignShop: string; //number, api
-  outStandingBalance: number; // do not post
-  wholeSaleDiscount: number; //discountAmount
+  role: role; // roleId, enum of role
+  assignShop: assignShop; //number, api
+  outStandingBalance?: number; // do not post
+  wholeSaleDiscount?: number; //discountAmount
   isActive: boolean;
-  password: string;
-  customerGroup: string; // number , customerGroupID, api neened
-  isPhnNumberAsUserNumber: boolean; // do not post
+  password?: string;
+  customerGroup: customerGroup; // number , customerGroupID, api neened
+  isPhnNumberAsUserNumber?: boolean; // do not post
   action?: any;
 }
