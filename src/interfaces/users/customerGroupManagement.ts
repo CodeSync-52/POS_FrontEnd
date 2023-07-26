550641c0d36fcac27783bc79285dac001c34b6e1
Moiz <Moiz190>
2023-07-26 07:32:19 -0700

    customer management finalized

---
+++ src/interfaces/users/customerGroupManagement.ts
@@ -0,0 +1,6 @@
+export interface ICustomerData {
+  id: number;
+  name: string;
+  status: string;
+  action: string;
+}
--- src/pages/customer-group/CustomerGroupManagement.vue
+++ src/pages/customer-group/CustomerGroupManagement.vue
@@ -2,25 +2,30 @@
   <div>
     <div class="flex justify-between items-center mb-4">
       <span class="text-3xl font-semibold">Customer Managment</span>
-      <q-btn label="Add New" icon="add" unelevated color="primary" />
+      <q-btn
+        label="Add New"
+        icon="add"
+        unelevated
+        color="primary"
+        @click="handleAddNewCustomerPopup"
+      />
     </div>
     <div class="q-pa-md">
       <q-table
         :rows="customerGroupRows"
-        :column="customerGroupColumns"
-        :filter="filter"
-        :visible-columns="['name', 'action']"
+        :columns="customerGroupColumns"
+        :visible-columns="['name', 'status', 'action']"
         :rows-per-page-options="['50', '5', '10', '15', '20']"
         row-key="name"
       >
         <template v-slot:body-cell-status="props">
           <q-td :props="props">
-            <div class="flex gap-2 justify-left md:pr-4">
+            <div class="flex gap-2 md:pr-4">
               <q-btn
                 size="sm"
                 flat
                 unelevated
-                :label="props.row.status"
+                :label="props.row.status === '1' ? 'Active' : 'InActive'"
                 @click="handleEditStatusPopup(props.row)"
               />
             </div>
@@ -28,7 +33,7 @@
         </template>
         <template v-slot:body-cell-action="props">
           <q-td class="!text-right" :props="props">
-            <div class="flex gap-2 justify-end md:pr-4">
+            <div class="flex gap-2 md:pr-4">
               <q-btn
                 size="sm"
                 flat
@@ -50,12 +55,12 @@
     </div>
 
     <q-dialog v-model="editStatusPopup">
-      <q-card class="q-pa-md">
+      <q-card class="q-pa-md full-width">
         <q-card-section>
-          <div class="text-h5 q-mb-md"><span> Edit Status </span></div>
-          <div class="q-gutter-md">
-            <q-radio v-model="selectedStatus" val="active" label="Active" />
-            <q-radio v-model="selectedStatus" val="inactive" label="InActive" />
+          <div class="text-h6 q-mb-md"><span> Edit Customer Status </span></div>
+          <div class="column">
+            <q-radio v-model="selectedStatus" val="1" label="Active" />
+            <q-radio v-model="selectedStatus" val="0" label="InActive" />
           </div>
         </q-card-section>
         <div class="row justify-end">
@@ -71,20 +76,37 @@
         </div>
       </q-card>
     </q-dialog>
-    <q-dialog v-model="editCustomerGroupNamePopup">
-      <q-card class="q-pa-md">
+    <q-dialog v-model="addCustomerGroupNamePopup">
+      <q-card class="q-pa-md full-width">
         <q-card-section>
-          <q-input v-model="editCustomerGroup.name" outlined label="Name" />
+          <div class="text-h6 q-mb-md">
+            <span>
+              {{
+                isEditCustomer ? 'Edit New Customer' : 'Add Customer Name'
+              }}</span
+            >
+          </div>
+          <q-input dense v-model="newCustomerName" outlined label="Name" />
         </q-card-section>
         <div class="row justify-end">
           <q-btn label="Cancel" flat unelevated color="red" v-close-popup />
           <q-btn
+            v-if="isEditCustomer"
             label="Save"
             flat
             unelevated
             color="signature"
             v-close-popup
             @click="handleSaveEditedCustomerGroupName"
+          />
+          <q-btn
+            v-else
+            label="Add"
+            flat
+            unelevated
+            color="signature"
+            v-close-popup
+            @click="handleSaveNewCustomer"
           />
         </div>
       </q-card>
@@ -94,149 +116,111 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import Swal from 'sweetalert2';
-const customerGroupColumns = ref([
+import { customerGroupMockRows } from './customerGroupMocks';
+import { ICustomerData } from '../../interfaces/users/customerGroupManagement';
+import { QTableColumn, useQuasar } from 'quasar';
+const $q = useQuasar();
+const customerGroupColumns = ref<QTableColumn<ICustomerData>[]>([
   {
     name: 'name',
     required: true,
-    label: 'Dessert (100g serving)',
+    label: 'Name',
     align: 'left',
     sortable: true,
+    field: 'name',
   },
   {
     name: 'status',
     label: 'Status',
     field: 'status',
-    align: 'center',
-    sortable: true,
+    align: 'left',
+    sortable: false,
+  },
+  {
+    name: 'action',
+    label: 'Action',
+    field: 'action',
+    align: 'left',
+    sortable: false,
   },
 ]);
-const filter = ref('');
-const selectedEditStatusRowIndex = ref(0);
-const selectedEditNameRowIndex = ref(0);
-const selectedStatus = ref('active');
-const editStatusPopup = ref(false);
-const editCustomerGroupNamePopup = ref(false);
-const editCustomerGroup = ref({
+const customerGroupRows = ref(customerGroupMockRows);
+const selectedRowData = ref<ICustomerData>({
+  id: 0,
   name: '',
+  status: '',
+  action: '',
 });
-const customerGroupRows = ref([
-  {
-    id: 0,
-    name: 'Frozen Yogurt',
-    status: 'Active',
-    action: 'edit',
-  },
-  {
-    id: 1,
-    name: 'Ice cream sandwich',
-    status: 'Active',
-    action: 'edit',
-  },
-  {
-    id: 2,
-    name: 'Eclair',
-    status: 'Active',
-    action: 'edit',
-  },
-  {
-    id: 3,
-    name: 'Cupcake',
-    status: 'Active',
-    action: 'edit',
-  },
-  {
-    id: 4,
-    name: 'Gingerbread',
-    status: 'Active',
-    action: 'edit',
-  },
-  {
-    id: 5,
-    name: 'Jelly bean',
-    status: 'Inactive',
-    action: 'edit',
-  },
-  {
-    id: 6,
-    name: 'Lollipop',
-    status: 'Inactive',
-    action: 'edit',
-  },
-  {
-    id: 7,
-    name: 'Honeycomb',
-    status: 'Inactive',
-    action: 'edit',
-  },
-  {
-    id: 8,
-    name: 'Donut',
-    status: 'Inactive',
-    action: 'edit',
-  },
-  {
-    id: 9,
-    name: 'KitKat',
-    status: 'Inactive',
-    action: 'edit',
-  },
-]);
-const handleEditStatusPopup = (selectedRow: {
-  id: number;
-  name: string;
-  status: string;
-  action: string;
-}) => {
-  selectedEditStatusRowIndex.value = customerGroupRows.value.findIndex(
-    (row) => selectedRow.id === row.id
-  );
-  if (selectedRow.status === 'Active') {
-    selectedStatus.value = 'active';
+const selectedStatus = ref('');
+const editStatusPopup = ref(false);
+const addCustomerGroupNamePopup = ref(false);
+const isEditCustomer = ref(false);
+const newCustomerName = ref('');
+const handleAddNewCustomerPopup = () => {
+  isEditCustomer.value = false;
+  newCustomerName.value = '';
+  addCustomerGroupNamePopup.value = true;
+};
+const handleSaveNewCustomer = () => {
+  const highestId =
+    Math.max(...customerGroupRows.value.map((customer) => customer.id)) + 1;
+  customerGroupRows.value = [
+    ...customerGroupRows.value,
+    {
+      id: highestId,
+      name: newCustomerName.value,
+      status: '1',
+      action: 'edit',
+    },
+  ];
+};
+const handleEditStatusPopup = (selectedRow: ICustomerData) => {
+  selectedRowData.value = selectedRow;
+  if (selectedRowData.value.status === '1') {
+    selectedStatus.value = '1';
   } else {
-    selectedStatus.value = 'inactive';
+    selectedStatus.value = '0';
   }
   editStatusPopup.value = true;
 };
 const handleSaveEditedStatus = () => {
-  customerGroupRows.value[selectedEditStatusRowIndex.value].status =
-    selectedStatus.value === 'active' ? 'Active' : 'InActive';
+  selectedRowData.value.status = selectedStatus.value;
 };
-const handleDeleteCustomerGroupRow = (selectedRow: {
-  id: number;
-  name: string;
-  status: string;
-  action: string;
-}) => {
-  const selectedRowIndex = customerGroupRows.value.findIndex(
-    (row) => selectedRow.id === row.id
-  );
-  Swal.fire({
-    title: 'Are you sure?',
-    text: "You won't be able to revert this!",
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes, delete it!',
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire('Deleted!', 'Your row has been deleted.', 'success');
-      return customerGroupRows.value.splice(selectedRowIndex, 1);
-    }
+const handleDeleteCustomerGroupRow = (selectedRow: ICustomerData) => {
+  $q.notify({
+    message: 'Are you sure you want to delete this record?',
+    color: 'signature',
+    actions: [
+      {
+        label: 'Delete',
+        color: 'yellow',
+        handler: () => {
+          const selectedRowIndex = customerGroupRows.value.findIndex(
+            (row) => selectedRow.id === row.id
+          );
+          customerGroupRows.value.splice(selectedRowIndex, 1);
+          $q.notify({
+            type: 'positive',
+            message: 'Your Selected Record is deleted.',
+          });
+        },
+      },
+      { label: 'Cancel', color: 'white' },
+    ],
   });
 };
-const handleEditCustomerGroupNamePopup = (selectedRow: {
-  id: number;
-  name: string;
-  status: string;
-  action: string;
-}) => {
-  editCustomerGroup.value.name = selectedRow.name;
-  editCustomerGroupNamePopup.value = true;
-  selectedEditNameRowIndex.value = selectedRow.id;
+const handleEditCustomerGroupNamePopup = (selectedRow: ICustomerData) => {
+  isEditCustomer.value = true;
+  newCustomerName.value = selectedRow.name;
+  addCustomerGroupNamePopup.value = true;
+  selectedRowData.value = selectedRow;
 };
 const handleSaveEditedCustomerGroupName = () => {
-  customerGroupRows.value[selectedEditNameRowIndex.value].name =
-    editCustomerGroup.value.name;
+  const filteredSelectedRecord = customerGroupRows.value.find(
+    (row) => selectedRowData.value.id === row.id
+  );
+  if (filteredSelectedRecord) {
+    filteredSelectedRecord.name = newCustomerName.value;
+  }
 };
 </script>
