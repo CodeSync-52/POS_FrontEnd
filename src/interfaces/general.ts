bca32bc25023b1ecf076b6cb37ee5af259982639
Wasiq <wasiqibnzahid@gmail.com>
2023-07-27 14:48:00 +0500

    W- Added permission validation, enums and role validation

--- src/interfaces/general.ts
+++ src/interfaces/general.ts
@@ -1,7 +1,3 @@
-export interface customerGroup {
-  value: number;
-  label: string;
-}
 export interface IOption<T = string | number> {
   label: string;
   value: T;
@@ -12,3 +8,110 @@ export type userManagementSearchFilter = {
   role: null | string | number;
   status: null | string | number;
 };
+
+export enum EUserRoles {
+  SuperAdmin = 'superadmin',
+  Admin = 'admin',
+  Customer = 'customer',
+  ShopManager = 'shopmanager',
+  ShopOfficer = 'shopofficer',
+  OfficeManager = 'officemanager',
+}
+
+export enum EActionPermissions {
+  View = 1,
+  Create = 2,
+  Update = 3,
+  Delete = 4,
+}
+
+export enum EUserModules {
+  UserManagment = 1,
+  RolePermission = 2,
+  CustomerGroupManagement = 3,
+  VariantManagement = 4,
+  CategoryManagement = 5,
+  ArticleManagement = 6,
+  ReceiptManagement = 7,
+  BillGeneration = 8,
+  CashInCashOutManagement = 9,
+  SalesManagement = 10,
+  InventoryManagement = 11,
+  ShopManagement = 12,
+  GoodsReceiptNotes = 13,
+  StockTransferRequests = 14,
+  SaleAndReturnManagement = 15,
+  ShopDiscountsModule = 16,
+  Report = 17,
+}
+
+export interface IRoleModule {
+  module: { moduleId: EUserModules; moduleName: string; moduleGroup: string };
+  actions: IRoleActionResponse[];
+}
+
+export interface IRoleActionResponse {
+  actionId: EActionPermissions;
+  actionName: string;
+}
+
+const roleDisplayList: Record<EUserRoles, string> = {
+  [EUserRoles.Admin]: 'Admin',
+  [EUserRoles.Customer]: 'Customer',
+  [EUserRoles.OfficeManager]: 'Office Manager',
+  [EUserRoles.ShopManager]: 'Shop Manager',
+  [EUserRoles.ShopOfficer]: 'Shop Officer',
+  [EUserRoles.SuperAdmin]: 'Super Admin',
+};
+
+const roleModuleList: Record<EUserModules, string> = {
+  [EUserModules.UserManagment]: 'User Management',
+  [EUserModules.RolePermission]: 'Role Permission',
+  [EUserModules.CustomerGroupManagement]: 'Customer Group Management',
+  [EUserModules.VariantManagement]: 'Variant Management',
+  [EUserModules.CategoryManagement]: 'Category Management',
+  [EUserModules.ArticleManagement]: 'Article Management',
+  [EUserModules.ReceiptManagement]: 'Receipt Management',
+  [EUserModules.BillGeneration]: 'Bill Generation',
+  [EUserModules.CashInCashOutManagement]: 'Cash In / Cash Out Management',
+  [EUserModules.SalesManagement]: 'Sales Management',
+  [EUserModules.InventoryManagement]: 'Inventory Management',
+  [EUserModules.ShopManagement]: 'Shop Management',
+  [EUserModules.GoodsReceiptNotes]: 'Goods Receipt Notes',
+  [EUserModules.StockTransferRequests]: 'Stock Transfer Requests',
+  [EUserModules.SaleAndReturnManagement]: 'Sale And Return Management',
+  [EUserModules.ShopDiscountsModule]: 'Shop Discounts',
+  [EUserModules.Report]: 'Report',
+};
+
+export function getRoleModuleName(key: EUserModules) {
+  return roleModuleList[key];
+}
+
+export function getRoleDisplayName(key: EUserRoles) {
+  return roleDisplayList[key];
+}
+
+export interface IUser {
+  aspNetUserId: string;
+  fullName: string;
+  userId: number;
+  loginTokenResponseDTO: LoginTokenResponseDTO;
+  rolePermissions: RolePermissions;
+}
+export interface LoginTokenResponseDTO {
+  access_Token: string;
+  access_Token_Expires_In: number;
+  token_Type: string;
+  userId: string;
+  refresh_Token?: null;
+  refresh_Token_Expires_In: number;
+  error?: null;
+  error_Description?: null;
+  message?: null;
+}
+export interface RolePermissions {
+  roleId: string;
+  roleName: string;
+  permissionModuleActions: IRoleModule[];
+}
