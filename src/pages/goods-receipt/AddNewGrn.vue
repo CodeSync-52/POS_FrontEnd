34ffc9b7c73be1cfd75afc13210830b24c77a307
moiz <moizahmad190@gmail.com>
2023-11-15 16:37:31 +0500

    lint error fixed

--- src/pages/goods-receipt/AddNewGrn.vue
+++ src/pages/goods-receipt/AddNewGrn.vue
@@ -315,13 +315,13 @@ window.addEventListener('keypress', async function (e: KeyboardEvent) {
               ...res.data.inventoryDetails[0],
               dispatchQuantity: 1,
             });
-          }
-          else {
+          } else {
             $q.notify({
-              message: 'Dispatch quantity can not be greater than avaible quantity.',
+              message:
+                'Dispatch quantity can not be greater than avaible quantity.',
               type: 'negative',
             });
-          }  
+          }
           scannedLabel.value = '';
           if (scannedLabelInput.value) scannedLabelInput.value.focus();
         }
