fe2f7f68dc818f74ef85f78ddbbfa31c3747077a
talha <baigtalha7@gmail.com>
2023-12-14 20:22:20 +0500

    New Changes Added

--- src/pages/return/SalesReturnManagement.vue
+++ src/pages/return/SalesReturnManagement.vue
@@ -66,7 +66,7 @@
 
 
     <div>
-      <div
+      <!-- <div
         class="flex md:flex-row md:gap-0 md:justify-between sm:justify-start sm:flex-col sm:gap-4 md:items-center sm:items-center mb-4">
         <span class="text-xl font-medium">{{ pageTitle }}</span>
         <div class="text-base flex items-center gap-1">
@@ -75,80 +75,172 @@
             {{ moment(timeStamp).format('YYYY-MM-DD') }}
           </span>
         </div>
-      </div>
+      </div> -->
       <div class="flex  justify-between">
-        <!-- bg-[#e3f1fe] -->
+        
         <div class="w-[80%]">
-          <div class="flex justify-between">
-            <div class="max-w-[300px] min-w-[250px]">
+          <div
+        class="flex md:flex-row md:gap-0 md:justify-between sm:justify-start sm:flex-col sm:gap-4 md:items-center sm:items-center mb-4">
+        <span class="text-xl font-medium">{{ pageTitle }}</span>
+        <div class="text-base flex items-center gap-1">
+          <!-- <span class="font-medium"> Date: </span> -->
+          <span class=" font-mono text-[18px] font-semibold">
+            {{ moment(timeStamp).format('LL')}}
+          </span>
+        </div>
+      </div>
+          <div class="flex justify-center md:justify-between">       
+            <div>
               <!-- <div class="flex justify-between items-center m-2 max-w-[300px]">
                 <span class="text-black text-[16px] font-medium">Shop</span>
                 <q-input class="max-w-[220px] min-w-[220px]" v-model="selectedShopBoyCode" outlined color="btn-primary"
                   dense>
                 </q-input>
               </div> -->
-              <q-input v-model="filterSearch.ProductCode" maxlength="250" class="my-3" outlined dense color="btn-primary"
-                label="Shop" />
+              <!-- <q-input v-model="filterSearch.ProductCode" maxlength="250" class="my-3" outlined dense color="btn-primary"
+                label="Shop" /> -->
+       
+      <q-select
+        popup-content-class="!max-h-[200px]"
+        class="my-3 mx-auto md:mx-0 max-w-[200px] min-w-[200px]"
+        use-input
+        dense
+        map-options
+        outlined
+        v-model="selectedShopBoyCode"
+      
+        label="User"
+        color="btn-primary"
+        option-label="name"
+        option-value="shopId"
+        ><template v-slot:no-option>
+          <q-item>
+            <q-item-section class="text-grey"> No results </q-item-section>
+          </q-item>
+        </template></q-select
+      >
+
               <!-- <div class="flex justify-between items-center m-2 max-w-[350px]">
                 <span class="text-black text-[16px] font-medium">Receipt</span>
                 <q-input class="max-w-[220px] min-w-[220px]" v-model="selectedShopBoyCode" outlined color="btn-primary"
                   dense>
                 </q-input>
               </div> -->
-              <q-input v-model="filterSearch.ProductCode" maxlength="250" class="my-3" outlined dense color="btn-primary"
-                label="Receipt" />
+              
               <!-- <div class="flex justify-between items-center m-2 max-w-[300px]">
                 <span class="text-black text-[16px] font-medium">User</span>
                 <q-input class="max-w-[220px] min-w-[220px]" v-model="selectedShopBoyCode" outlined color="btn-primary"
                   dense>
                 </q-input>
               </div> -->
-              <q-input v-model="filterSearch.ProductCode" maxlength="250" class="my-3" outlined dense color="btn-primary"
-                label="User" />
+
+
+              <!-- <q-input v-model="filterSearch.ProductCode" maxlength="250" class="my-3" outlined dense color="btn-primary"
+                label="User" /> -->
+                <!-- <q-select
+        popup-content-class="!max-h-[200px]"
+        class="max-w-[220px] min-w-[220px] my-3 mx-auto md:mx-0"
+        use-input
+        dense
+        map-options
+        outlined
+        v-model="filterSearch.ShopId"
+        @update:model-value="filterSearch.ShopId = $event.shopId"
+        label="User"
+        color="btn-primary"
+        option-label="name"
+        option-value="shopId"
+        ><template v-slot:no-option>
+          <q-item>
+            <q-item-section class="text-grey"> No results </q-item-section>
+          </q-item>
+        </template></q-select> -->
+            </div>
+            <div>
+              <!-- max-w-[220px] min-w-[220px] -->
+
+
+                <!-- <q-select
+        popup-content-class="!max-h-[200px]"
+        class=" my-3 mx-auto md:mx-0"
+        use-input
+        dense
+        map-options
+        outlined
+        v-model="selectedShop.fromShop"
+        @update:model-value="handleUpdateShop($event)"
+        label="Shop"
+        color="btn-primary"
+        option-label="name"
+        option-value="shopId"
+        ></q-select> -->
+
+        <q-select
+        :loading="isFetchingShopList"
+        popup-content-class="!max-h-[200px]"
+        :options="shopData"
+        class="my-3 max-w-[200px] min-w-[200px]"
+        use-input
+        dense
+        map-options
+        outlined
+        v-model="selectedShop.fromShop"
+        @update:model-value="handleUpdateShop($event)"
+        label="Select Shop"
+        color="btn-primary"
+        option-label="name"
+        option-value="shopId"
+      ></q-select>
+
             </div>
             <div class="max-w-[300px] min-w-[250px]">
-              <!-- <div class="flex justify-between items-center m-2 max-w-[380px]">
-                <span class="text-black text-[16px] font-medium">Remaining Balance</span>
-                <q-input class="max-w-[220px] min-w-[220px]" v-model="selectedShopBoyCode" outlined color="btn-primary"
-                  dense>
-                </q-input>
-              </div> -->
+              <q-input v-model="filterSearch.ProductCode" maxlength="250" class="my-3" outlined dense color="btn-primary"
+                label="Receipt" />
+            </div>
+            <div class="max-w-[300px] min-w-[250px]">
               <q-input v-model="filterSearch.ProductCode" maxlength="250" class="my-3" outlined dense color="btn-primary"
                 label="Remaining Balance" />
-              <!-- <div class="flex justify-between items-center m-2 max-w-[380px]">
-                <span class="text-black text-[16px] font-medium">Global Discount</span>
-                <q-input class="max-w-[220px] min-w-[220px]" v-model="selectedShopBoyCode" outlined color="btn-primary"
-                  dense>
-                </q-input>
-              </div> -->
-              <q-input v-model="filterSearch.ProductCode" maxlength="250" class="my-3" outlined dense color="btn-primary"
-                label="Global Discount" />
             </div>
+            
           </div>
           <div class="py-4">
-            <q-table :columns="InventoryListColumn" :rows="InventoryListRecords" :loading="isLoading"
-              v-model:pagination="pagination" @request="getInventoryList"><template v-slot:no-data>
-                <div class="mx-auto q-pa-sm text-center row q-gutter-x-sm">
-                  <q-icon name="warning" size="xs" />
-                  <span class="text-md font-medium"> No data available. </span>
-                </div>
-              </template>
-              <template v-slot:body-cell-productImage="props">
-                <q-td :props="props">
-                  <div @click="handlePreviewImage(props.row.productImage)"
-                    class="h-[50px] w-[50px] min-w-[2rem] overflow-hidden rounded-full"
-                    :class="props.row.productImage ? 'cursor-pointer' : ''">
-                    <img class="w-full h-full object-cover" :src="getImageUrl(props.row.productImage) ||
-                      'assets/default-image.png'
-                      " alt="img" />
-                  </div>
-                </q-td>
-              </template>
-            </q-table>
+      <q-table
+        :rows="ShopDetailRecords"
+        v-model:pagination="pagination"
+        @request="getInventoryDetailList"
+        :columns="InventoryListColumn"
+        :loading="isFetchingRecords"
+        class="max-h-[47vh] 3xl:max-h-[58vh]"
+      >
+        <template v-slot:no-data>
+          <div class="mx-auto q-pa-sm text-center row q-gutter-x-sm">
+            <q-icon name="warning" size="xs" />
+            <span class="text-md font-medium"> No data available. </span>
           </div>
-           <div class="w-full flex justify-end">
+        </template>
+        <template v-slot:body-cell-productImage="props">
+          <q-td :props="props">
+            <div
+              @click="handlePreviewImage(props.row.productImage)"
+              class="h-[50px] w-[50px] min-w-[2rem] overflow-hidden rounded-full"
+              :class="props.row.productImage ? 'cursor-pointer' : ''"
+            >
+              <img
+                class="w-full h-full object-cover"
+                :src="
+                  getImageUrl(props.row.productImage) ||
+                  'assets/default-image.png'
+                "
+                alt="img"
+              />
+            </div>
+          </q-td>
+        </template>
+      </q-table>
+    </div>
+           <div class="w-full flex justify-center md:justify-end">
             <div class="max-w-[300px] min-w-[250px]">
-              <q-input v-model="filterSearch.ProductCode" maxlength="250" class="my-3" outlined dense color="btn-primary"
+              <q-input v-model="filterSearch.ProductCode" maxlength="250" class="my-2" outlined dense color="btn-primary"
                 label="Total" />
               <q-input v-model="filterSearch.ProductCode" maxlength="250" class="my-3" outlined dense color="btn-primary"
                 label="Discount" />
@@ -156,74 +248,55 @@
                 label="Net Total" />
             </div>
            </div>
-          <!-- <q-input class="max-w-[220px] min-w-[220px] m-2" v-model="selectedShopBoyCode" outlined color="btn-primary" dense>
-        <template v-slot:before>
-          <span class="text-black text-[16px] mr-[0.25rem] font-medium">Receipt</span>
-        </template>
-        </q-input>
-            <q-input class="max-w-[220px] min-w-[220px] m-2" v-model="selectedShopBoyCode" outlined color="btn-primary" dense>
-        <template v-slot:before>
-          <span class="text-black text-[16px] mr-[1.50rem] font-medium">User</span>
-        </template>
-        </q-input> -->
-
         </div>
-        <div class="w-[17%]">
-          <!-- <q-btn
-          unelevated
-          :loading="isLoading"
-          color=""
-          class="rounded-[4px] h-2 border bg-btn-primary hover:bg-btn-primary-hover w-full h-[50px]"
-          icon="search"
-          label="Search"
-          @click="getInventoryList()"
-        /> -->
+        <div class="w-[17%] relative">
+        <div class="sticky top-20">
         <q-btn unelevated
           color=""
-          class="rounded-[4px] h-2 border bg-btn-primary hover:bg-btn-primary-hover w-full h-[60px] my-3">
-      <q-icon left size="3em" name="map" />
-      <div class="text-lg">Exit</div>
-    </q-btn>
+          class="rounded-[8px] border icon_left bg-btn-primary hover:bg-btn-primary-hover w-full py-2 lg:py-5 my-3 text-[14px] lg:text-[16px]" label="Cancel Receipt" icon="close" />
         <q-btn unelevated
           color=""
-          class="rounded-[4px] h-2 border bg-btn-primary hover:bg-btn-primary-hover w-full h-[60px] my-3">
-      <q-icon left size="3em" name="close" />
-      <div class="text-lg">Cancel</div>
-    </q-btn>
+          class="rounded-[8px] border icon_left bg-btn-primary hover:bg-btn-primary-hover w-full py-2 lg:py-5 my-3 text-[14px] lg:text-[16px]" label="Create Return" icon="cached" />
         <q-btn unelevated
           color=""
-          class="rounded-[4px] h-2 border bg-btn-primary hover:bg-btn-primary-hover w-full h-[60px] my-3">
-      <q-icon left size="3em" name="cached" />
-      <div class="text-lg">Return</div>
-    </q-btn>
+          class="rounded-[8px] border icon_left bg-btn-primary hover:bg-btn-primary-hover w-full py-2 lg:py-5 my-3 text-[14px] lg:text-[16px]" label="Hold Sale" icon="hourglass_empty" />
         <q-btn unelevated
           color=""
-          class="rounded-[4px] h-2 border bg-btn-primary hover:bg-btn-primary-hover w-full h-[60px] my-3">
-      <q-icon left size="3em" name="hourglass_empty" />
-      <div class="text-lg">Hold Sale</div>
-    </q-btn>
+          class="rounded-[8px] border icon_left bg-btn-primary hover:bg-btn-primary-hover w-full py-2 lg:py-5 my-3 text-[14px] lg:text-[16px]" label="Show Hold Sale" icon="visibility" />
         <q-btn unelevated
           color=""
-          class="rounded-[4px] h-2 border bg-btn-primary hover:bg-btn-primary-hover w-full h-[60px] my-3">
-      <q-icon left size="3em" name="trending_up" />
-      <div class="text-lg">Today Sale</div>
-    </q-btn>
+          class="rounded-[8px] border icon_left bg-btn-primary hover:bg-btn-primary-hover w-full py-2 lg:py-5 my-3 text-[14px] lg:text-[16px]" label="Today sale (f9)" icon="trending_up" />
         <q-btn unelevated
           color=""
-          class="rounded-[4px] h-2 border bg-btn-primary hover:bg-btn-primary-hover w-full h-[60px] my-3">
-      <q-icon left size="3em" name="paid" />
-      <div class="text-lg">Balance</div>
-    </q-btn>
+          class="rounded-[8px] border icon_left bg-btn-primary hover:bg-btn-primary-hover w-full py-2 lg:py-5 my-3 text-[14px] lg:text-[16px]" label="Close Balance" icon="paid" />
         <q-btn unelevated
           color=""
-          class="rounded-[4px] h-2 border bg-btn-primary hover:bg-btn-primary-hover w-full h-[60px] my-3">
-      <q-icon left size="3em" name="shopping_cart" />
-      <div class="text-lg">Save</div>
+          class="rounded-[8px] icon_left border bg-btn-primary hover:bg-btn-primary-hover w-full py-2 lg:py-5 my-3 text-[14px] lg:text-[16px]" label="Save" icon="shopping_cart" />
+           <!-- class="rounded-[8px] border bg-btn-primary hover:bg-btn-primary-hover w-full py-2 lg:py-5 my-3 text-[14px] lg:text-[16px]" label="Save" icon="shopping_cart" /> -->
+
+           <!-- <q-btn unelevated
+           color=""
+           class="rounded-[8px] border bg-btn-primary hover:bg-btn-primary-hover my-5 w-full py-2 ">
+           <div class="flex items-center w-full justify-between mx-7">
+      <q-icon size="3em" name="map" />
+      <div class="text-[14px] lg:text-[16px]">Save</div>
+    </div>
     </q-btn>
+           <q-btn unelevated
+           color=""
+           class="rounded-[8px] border bg-btn-primary hover:bg-btn-primary-hover w-full py-2 ">
+           <div class="flex items-center">
+            <div class="w-[50px]">
+      <q-icon size="3em" name="map" />
+    </div>
+      <div class="text-[14px] lg:text-[16px]">Save optp</div>
+    </div>
+    </q-btn> -->
+
         </div>
       </div>
+      </div>
     </div>
-
 
     <!-- <div
       class="row flex lg:justify-end sm:justify-start items-center w-full min-h-[3.5rem] gap-8"
@@ -343,6 +416,13 @@
     </q-dialog> -->
   </div>
 </template>
+<style scoped lang="scss">
+.icon_left>:nth-child(2) {
+  justify-content: flex-start;
+  gap: 20px;
+  margin-left: 15px;
+}
+</style>
 
 <script setup lang="ts">
 import moment from 'moment';
@@ -357,7 +437,7 @@ import {
 import { inventoryDetailApi, shopListApi } from 'src/services';
 import { useAuthStore } from 'src/stores';
 import { isPosError } from 'src/utils';
-// import { InventoryListColumn } from 'src/utils/inventory';
+import { InventoryListColumn } from 'src/utils/inventory';
 import { ref, onMounted, onUnmounted } from 'vue';
 const authStore = useAuthStore();
 const timeStamp = Date.now();
@@ -388,8 +468,8 @@ const $q = useQuasar();
 const isFetchingShopList = ref(false);
 const isFetchingRecords = ref(false);
 const apiController = ref<AbortController | null>(null);
-// const isPreviewImageModalVisible = ref(false);
-// const selectedPreviewImage = ref('');
+const isPreviewImageModalVisible = ref(false);
+const selectedPreviewImage = ref('');
 
 onMounted(() => {
   getShopList();
@@ -411,22 +491,22 @@ onUnmounted(() => {
     apiController.value.abort();
   }
 });
-// const handleUpdateShop = (newVal: IShopResponse) => {
-//   selectedShop.value.fromShop = newVal;
-//   filterSearch.value.ShopId = newVal.shopId;
-// };
-// const handlePreviewImage = (selectedImage: string) => {
-//   if (selectedImage) {
-//     selectedPreviewImage.value = `data:image/png;base64,${selectedImage}`;
-//     isPreviewImageModalVisible.value = true;
-//   }
-// };
-// const getImageUrl = (base64Image: string | null) => {
-//   if (base64Image) {
-//     return `data:image/png;base64,${base64Image}`;
-//   }
-//   return '';
-// };
+const handleUpdateShop = (newVal: IShopResponse) => {
+  selectedShop.value.fromShop = newVal;
+  filterSearch.value.ShopId = newVal.shopId;
+};
+const handlePreviewImage = (selectedImage: string) => {
+  if (selectedImage) {
+    selectedPreviewImage.value = `data:image/png;base64,${selectedImage}`;
+    isPreviewImageModalVisible.value = true;
+  }
+};
+const getImageUrl = (base64Image: string | null) => {
+  if (base64Image) {
+    return `data:image/png;base64,${base64Image}`;
+  }
+  return '';
+};
 const getShopList = async () => {
   isFetchingShopList.value = true;
   try {
