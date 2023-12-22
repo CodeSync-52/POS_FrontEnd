a2846dfd3fa94195ed3ed764fce8ffe8ddb96e55
talha <baigtalha7@gmail.com>
2023-12-22 19:35:17 +0500

    Working in Remaining Balance Page

--- src/pages/return/RemainingBalance.vue
+++ src/pages/return/RemainingBalance.vue
@@ -5,29 +5,69 @@
         <div class="font-semibold text-lg text-center">
           <span> Remaining Balance Details</span>
         </div>
-        <div class="flex flex-col gap-2 md:gap-4 items-center md:items-end q-pa-md">
+        <div
+          class="flex flex-col gap-2 md:gap-4 items-center md:items-end q-pa-md"
+        >
           <div class="md:flex gap-2 items-center">
             <span class="font-medium md:text-lg">Opening Balance :</span>
-            <q-input v-model="text" type="number" class="max-w-[200px] min-w-[200px]" maxlength="250" disable outlined
-              dense color="btn-primary" />
+            <q-input
+              v-model="text"
+              type="number"
+              class="max-w-[200px] min-w-[200px]"
+              maxlength="250"
+              disable
+              outlined
+              dense
+              color="btn-primary"
+            />
           </div>
           <div class="md:flex gap-2 items-center">
             <span class="font-medium md:text-lg">Running Balance :</span>
-            <q-input v-model="text" type="number" class="max-w-[200px] min-w-[200px]" maxlength="250" disable outlined
-              dense color="btn-primary" />
+            <q-input
+              v-model="text"
+              type="number"
+              class="max-w-[200px] min-w-[200px]"
+              maxlength="250"
+              disable
+              outlined
+              dense
+              color="btn-primary"
+            />
           </div>
           <div class="md:flex gap-2 items-center">
             <span class="font-medium md:text-lg">Balance Submit to HOD :</span>
-            <q-input v-model="text" type="number" class="max-w-[200px] min-w-[200px]" maxlength="250" disable outlined
-              dense color="btn-primary" />
+            <q-input
+              v-model="text"
+              type="number"
+              class="max-w-[200px] min-w-[200px]"
+              maxlength="250"
+              disable
+              outlined
+              dense
+              color="btn-primary"
+            />
           </div>
         </div>
         <div class="q-pa-md">
-          <q-table flat bordered title="Expenses" :rows="rows" :columns="columns" row-key="id" :loading="loading">
+          <q-table
+            flat
+            bordered
+            title="Expenses"
+            :rows="rows"
+            :columns="columns"
+            row-key="id"
+            :loading="loading"
+          >
             <template v-slot:bottom-row>
               <q-tr>
                 <q-td>
-                  <q-btn :disable="loading" unelevated color="btn-primary" label="Add row" @click="addRow" />
+                  <q-btn
+                    :disable="loading"
+                    unelevated
+                    color="btn-primary"
+                    label="Add row"
+                    @click="addRow"
+                  />
                 </q-td>
               </q-tr>
             </template>
@@ -39,12 +79,12 @@
 </template>
 <script lang="ts" setup>
 import { QTableColumn } from 'quasar';
-import { ref } from 'vue'
-const text = ref('')
+import { ref } from 'vue';
+const text = ref('');
 interface IExpenses {
-  id: number,
-  name: string,
-  amount: number
+  id: number;
+  name: string;
+  amount: number;
 }
 const columns: QTableColumn<IExpenses>[] = [
   {
@@ -52,16 +92,16 @@ const columns: QTableColumn<IExpenses>[] = [
     required: true,
     label: 'Expense Name',
     align: 'left',
-    field: row => row.name,
+    field: (row) => row.name,
   },
   {
     name: 'amount',
     required: true,
     label: 'Amount',
     align: 'left',
-    field: row => row.amount,
+    field: (row) => row.amount,
   },
-]
+];
 const originalRows = [
   {
     id: 0,
@@ -72,12 +112,11 @@ const originalRows = [
     id: 1,
     name: 'Ice',
     amount: 10,
-  }
-]
-const rows = ref([...originalRows])
+  },
+];
+const rows = ref([...originalRows]);
 const loading = ref(false);
 const rowCount = ref(rows.value.length);
-
 const addRow = () => {
   const newId = rowCount.value++;
   const newRow: IExpenses = {
