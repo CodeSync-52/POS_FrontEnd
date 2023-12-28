4f45bc873fd2114913442276d5d7a69aa53971a0
talha <baigtalha7@gmail.com>
2023-12-28 18:19:57 +0500

    (Sale Added Api) and Required Functionality added in sales and return managment page

--- src/interfaces/sale-and-return-management.ts
+++ src/interfaces/sale-and-return-management.ts
@@ -5,3 +5,21 @@ export interface IShopSaleExpenses {
   isSavingRow: boolean;
   isEditing: boolean;
 }
+export interface IAddShopSaleManagement {
+  salePersonCode: string | null;
+  shopId: number | undefined;
+  totalDiscount: number;
+  saleStatus: number;
+  comments: string;
+  saleDetails: ISaleDetail[];
+}
+
+export interface ISaleDetail {
+  inventoryId: number;
+  saleId: number;
+  productId: number;
+  productCode: string;
+  amount: number;
+  quantity: number;
+  discount: number;
+}
